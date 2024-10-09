5a5dd0a3d1337d23c3cf3aaadac95afaae005b77
SENA <SENA@ARMSCCCCTGOD188.FormacionSena.RED>
2024-10-09 09:22:23 -0500

    taller terminado

---
+++ app.js
@@ -0,0 +1,60 @@
+const name = document.getElementById('name');
+const note1 = document.getElementById('note1');
+const note2 = document.getElementById('note2');
+const note3 = document.getElementById('note3');
+const btnCalculate = document.getElementById('btn-calculate');
+const btnPredict = document.getElementById('btn-predict');
+const result = document.getElementById('result');
+
+btnCalculate.addEventListener('click', calculateNote);
+
+function calculateNote(event) {
+    event.preventDefault();
+    let userName = name.value;
+    let data1 = Number(note1.value);
+    let data2 = Number(note2.value);
+    let data3 = Number(note3.value);
+    
+    let result1 = ((data1 * 0.3) + (data2 * 0.3) + (data3 * 0.4)).toFixed(1);
+    
+    if (result1 >= 1 && result1 < 3.5) {
+        result.style.color = 'black';
+        result.textContent = `${userName} has perdido la materia con ${result1}`;
+    } else if (result1 >= 3.5 && result1 < 4.5) {
+        result.style.color = 'orange';
+        result.textContent = `${userName} has ganado la materia con ${result1}`;
+    } else if (result1 >= 4.5 && result1 <= 5) {
+        result.style.color = 'green';
+        result.textContent = `${userName} has ganado la materia con ${result1} (sobresaliente)`;
+    } else {
+        alert('Alguna de las notas no concuerda');
+    }
+}
+
+btnPredict.addEventListener('click', predictNote);
+
+function predictNote(event) {
+    event.preventDefault();
+    let userName = name.value;
+    let data1 = Number(note1.value);
+    let data2 = Number(note2.value);
+    let notaMinima = 3.5;
+
+    if (data1 < 1 || data1 > 5 || data2 < 1 || data2 > 5) {
+        alert('Alguna de las notas no concuerda');
+        return;
+    }
+
+    let result2 = (notaMinima - (data1 * 0.3) - (data2 * 0.3)) / 0.4;
+
+    if (result2 > 5.0) {
+        result.style.color = 'black';
+        result.textContent = `${userName}, te falta mucho para pasar la materia. Necesitas: ${result2.toFixed(2)}`;
+    } else if (result2 < 0) {
+        result.style.color = 'green';
+        result.textContent = `${userName}, ya tienes suficiente para pasar la materia.`;
+    } else {
+        result.style.color = 'blue';
+        result.textContent = `${userName}, necesitas sacar al menos: ${result2.toFixed(2)} para pasar la materia.`;
+    }
+}
